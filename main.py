35d8de477c63bd6d7560305a3087d80ec594a622
khubiarov <72666523+khubiarov@users.noreply.github.com>
2023-05-12 14:01:14 +0800

    Almost complete project

--- main.py
+++ main.py
@@ -129,26 +129,41 @@ class SJApi(ReqFromApi):
 
 
 def read_file(file_name):
-    i = 0
-    with open(f"{file_name}", 'rt', encoding='utf-8') as r_file:
-        content = csv.reader(r_file, delimiter=",")
-        for line in content:
-            i += 1
-            if i % 10 == 0:
-                numb_of_vacancy = int(input('Какую вакансию показать подробнее?\nДальше?"N"'))
-                print(all_vacancies_copyes[numb_of_vacancy - 1])
-                input('дальше')
+    while True:
+        i = 0
+        with open(f"{file_name}", 'rt', encoding='utf-8') as r_file:
+            content = csv.reader(r_file, delimiter=",")
+            for line in content:
+                i += 1
 
-            if line[4] is None:
-                line_4 = '--'
-            else:
-                line_4 = line[4]
+                if line[4] is None:
+                    line_4 = '--'
+                else:
+                    line_4 = line[4]
 
                 #создаем копии
-            all_vacancies_copyes.append(CopyofVacancy(i, line[0], line[1], line[2], line[3], line_4, line[5], line[6]))
-            print(f"{i}) {line[0]}; {line[1]};от {line[2]} до {line[3]} {line[4]} ")
+                all_vacancies_copyes.append(CopyofVacancy(i, line[0], line[1], line[2], line[3], line_4, line[5], line[6]))
+                print(f"{i}) {line[0]}; {line[1]};от {line[2]} до {line[3]} {line[4]} ")
+
+                if i % 10 == 0:
+                    numb_of_vacancy = input('Какую вакансию показать подробнее?\n"N" - Дальше\n"Q" - Выход\n')
+                    if numb_of_vacancy.isdigit():
+                        if int(numb_of_vacancy) > i:
+                            print('Ошибка , нет такой вакансии')
+                            break
+                        else:
+                            print(all_vacancies_copyes[int(numb_of_vacancy) - 1])
+                            input('нажмите enter')
+                            break
+                    elif numb_of_vacancy == 'N'.lower():
+                        continue
+                    elif numb_of_vacancy == 'Q'.lower():
+                        exit()
 
 
+                    else:
+                        print('Ошибка,нет такой команды')
+                        break
 
 all_vacancies_copyes = []
 
@@ -168,5 +183,6 @@ for copy in copies_api_req:
     copy.get_vacancies()
     copy.make_file()
 
+while True:
+    read_file(usr_inp_file)
 
-read_file(usr_inp_file)
